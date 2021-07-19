d612512d72154d2767823427fb92cff9df9ee4d8
Luke Mueller <lukemueller8@gmail.com>
2021-07-19 11:15:11 -0500

    rearranges user model fields

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  password: { type: String, required: true },
   username: { type: String, required: true },
+  password: { type: String, required: true },
   activeLists: [
     {
       _id: { type: Schema.Types.ObjectId, ref: "List" },
