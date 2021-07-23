b4f21188d55039cfe688ddae5946b9c429467556
Luke Mueller <lukemueller8@gmail.com>
2021-07-23 16:36:19 -0500

    removes unnecessary route

--- routes/lists.js
+++ routes/lists.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const listsController = require('../controllers/lists');
+const listsController = require("../controllers/lists");
 
 const router = express.Router();
 
-router.put('/archiveList', listsController.archiveList)
-router.delete('/deleteList/:listId/:userId', listsController.deleteList);
-router.put('/editList', listsController.putList)
-router.get('/getLists/:arr', listsController.getLists);
-router.post('/postList', listsController.postList);
+router.put("/archiveList", listsController.archiveList);
+router.delete("/deleteList/:listId/:userId", listsController.deleteList);
+router.put("/editList", listsController.putList);
+router.get("/getLists/:arr", listsController.getLists);
+router.post("/postList", listsController.postList);
 
 module.exports = router;
