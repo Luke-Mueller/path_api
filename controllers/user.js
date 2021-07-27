22f2da97dc3af361ec1ec114b83bb092772bfbe8
Luke Mueller <lukemueller8@gmail.com>
2021-07-27 16:20:14 -0500

    removes activeList routes from user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,54 +1,5 @@
 const User = require("../models/User");
 
-exports.activateList = async (req, res, next) => {
-  const list = req.body.list;
-  const userId = req.body.userId;
-
-  try {
-    const user = await User.findById(userId);
-    if (user) {
-      if (!user.activeLists.find((el) => el._id.toString() === list._id)) {
-        const newItems = list.items.map((listItem) => {
-          if (listItem.itemType === "sublist") {
-            const newSubItems = listItem.subItems.map((subItem) => {
-              return {
-                item: subItem,
-                done: false,
-              };
-            });
-            listItem.subItems = newSubItems;
-          }
-          return {
-            ...listItem,
-            done: false,
-          };
-        });
-        const newActiveList = {
-          _id: list._id,
-          items: newItems,
-          progress: 0,
-        };
-        user.activeLists.push(newActiveList);
-        const savedUser = await user.save();
-        res.status(200).json({
-          message: "The list was added to active lists successfully!",
-          user: savedUser,
-        });
-      }
-    } else {
-      const error = new Error();
-      error.message = "The user trying to start a list does not exist.";
-      error.statusCode = 404;
-      error.title = "No user found...";
-      throw error;
-    }
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.deactivateList = async (req, res, next) => {};
-
 exports.login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.params.username });
