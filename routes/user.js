da6968d20b8489a641ee73e00956bb8e74b179cc
Luke Mueller <lukemueller8@gmail.com>
2021-07-27 16:21:41 -0500

    removes old routes

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,6 @@ const userController = require("../controllers/user");
 
 const router = express.Router();
 
-router.put("/activateList", userController.activateList);
-router.put("/deactivateList", userController.deactivateList);
 router.get("/login/:username", userController.login);
 router.post("/signup", userController.signup);
 
