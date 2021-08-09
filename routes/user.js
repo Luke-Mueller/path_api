45aba78a299e65fec92af2216e46b31707678a83
Luke Mueller <lukemueller8@gmail.com>
2021-08-09 16:50:19 -0500

    adds put req to user

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/login/:username", userController.login);
 router.post("/signup", userController.signup);
+router.put("/restoreList", userController.restoreList);
 
 module.exports = router;
