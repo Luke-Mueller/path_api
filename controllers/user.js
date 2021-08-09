59f9ddcda10ba4b55fced71cb48963f683f55e14
Luke Mueller <lukemueller8@gmail.com>
2021-08-09 16:49:46 -0500

    adds restore list auth controller

--- controllers/user.js
+++ controllers/user.js
@@ -20,6 +20,36 @@ exports.login = async (req, res, next) => {
   }
 };
 
+exports.restoreList = async (req, res, next) => {
+  const listId = req.body.listId;
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findById(userId);
+  
+    // remove listId from user.archivedLists
+    const newArchivedLists = user.archivedLists.filter(
+      (list) => list._id.toString() !== listId.toString()
+    );
+    user.archivedLists = newArchivedLists;
+  
+    // push listId to user.myLists
+    const newMyLists = [...user.myLists];
+    newMyLists.push(listId);
+    user.myLists = newMyLists;
+  
+    // save user
+    const returnedUser = await user.save();
+    // return user in res
+    res.status(200).json({
+      message: "List retored!",
+      user: returnedUser,
+    });
+  } catch (error) {
+    next(error)
+  }
+};
+
 exports.signup = async (req, res, next) => {
   const existingUser = await User.findOne({ username: req.body.username });
   if (!existingUser) {
